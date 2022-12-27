a44163292e5cf800e3b9708c09cf8ad6f8d71b67
Jihad Omer <jhjahony@gmail.com>
2022-12-27 13:02:43 +0000

    firsr commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <ul>
+    <li className='boy'>Boy</li>
+    <li className='boy'>Boy</li>
+    <li className='boy'>Boy</li>
+    <li className='girl'>girl</li>
+    <li className='girl'>girl</li>
+    <li className='girl'>girl</li>
+   </ul>
+
+
+      
+    </div>
+  );
+}
+
+export default App;
