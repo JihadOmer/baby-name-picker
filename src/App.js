f71d476902c12b4a5b838009888103a1ad3d058e
Jihad Omer <jhjahony@gmail.com>
2022-12-28 19:59:02 +0000

    Add shortlist component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import data from "./data/data";
 import Nameslist from "./components/NamesList";
 import Footer from "./components/Footer";
 import Search from "./components/Search";
+import ShortList from "./components/ShortList";
+
+
+
+
 function App() {
   return (
     <div className="App">
       <main>
         <Search />
+        <ShortList/>
         <Nameslist data={data} />
       </main>
 
---
+++ src/components/ShortList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShortList = () => {
+  return (
+    <p>Add names to as a Shortlist ... </p>
+  )
+}
+
+export default ShortList
