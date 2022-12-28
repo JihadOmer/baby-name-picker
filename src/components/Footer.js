5ec5d9da50adab4e3fb77b1bc2319a03c6c625c9
Jihad Omer <jhjahony@gmail.com>
2022-12-28 19:27:41 +0000

    Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <div className="credit">
+      <p>
+
+        This application idea, and look, were taken from
+        <a href="https://www.yahoo.com/news/atticus-tops-baby-names-2015-124073377716.html">
+          {"  "}
+          Simon Vrachliotis
+        </a>
+        {"  "}
+        app, found via react.rocks.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
