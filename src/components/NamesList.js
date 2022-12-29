6bbd007b4d7efa67f82072708ed1fbc75d44df1e
Jihad Omer <jhjahony@gmail.com>
2022-12-29 20:53:05 +0000

    test commite

--- src/components/NamesList.js
+++ src/components/NamesList.js
@@ -1,34 +1,26 @@
 import React from "react";
 
 const NamesList = (props) => {
+
+
   const { data, filterText } = props;
 
-  
 
-  const nameslist = data
-  // remove the names that do not match the search text
+        const nameslist = data
+       .filter((name) => {
+         // remove the names that do not match the search text
+         return name.name.toLowerCase().indexOf(filterText.toLowerCase()) >= 0;
+       })
 
-    .filter((name) => {
-      return name.name.toLowerCase().indexOf(filterText.toLowerCase()) >= 0;
-    })
-
-    .map((name) => {
-
-      return (
-
-        <li key={name.id} className={name.sex}>
-          {name.name}
-        </li>
-      );
-    });
-
-
-
-
+       .map((name) => {
+         return (
+           <li key={name.id} className={name.sex}>
+             {name.name}
+           </li>
+         );
+       });
 
   return <div className="App">{nameslist}</div>;
-
 };
-
 
 export default NamesList;
